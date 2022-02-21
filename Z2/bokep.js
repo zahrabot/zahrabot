23aed7d75f1bf77a147d54bebcc622165f72a02f
zahrabot <irwanasu090>
2022-02-21 14:49:31 +0700

    v6

---
+++ Z2/bokep.js
@@ -0,0 +1,3 @@
+//ngapain di buka?
+//subrek Ramdani official lah tod di mana hati kalian ini:)
+//dah capek capem coding demi kalian, eh ujung2 nya gak di subrek:)
---
+++ Z5/menunya.js
@@ -0,0 +1,378 @@
+
+╭─⬣「 *GROUP MENU* 」⬣
+│❏ヅ *${prefix}hidetag*
+│❏ヅ *${prefix}leave*
+│❏ヅ *${prefix}tagtime*
+│❏ヅ *${prefix}delete*
+│❏ヅ *${prefix}tagall*
+│❏ヅ *${prefix}tagall2*
+│❏ヅ *${prefix}tagall3*
+│❏ヅ *${prefix}tagall4*
+│❏ヅ *${prefix}tagall5*
+│❏ヅ *${prefix}linkgrub*
+│❏ヅ *${prefix}setdesc*
+│❏ヅ *${prefix}listadmin*
+│❏ヅ *${prefix}setname*
+│❏ヅ *${prefix}group*
+│❏ヅ *${prefix}listonline*
+│❏ヅ *${prefix}add*
+│❏ヅ *${prefix}kick*
+│❏ヅ *${prefix}antilink*
+│❏ヅ *${prefix}antivirtex*
+│❏ヅ *${prefix}antitag*
+│❏ヅ *${prefix}antitoxic*
+│❏ヅ *${prefix}kickrandom*
+│❏ヅ *${prefix}sms*
+└⬣
+
+╭─⬣「 *WALLPAPER* 」⬣
+│❏ヅ *${prefix}wallrandom* 
+│❏ヅ *${prefix}wallcode*
+│❏ヅ *${prefix}wallneon*
+│❏ヅ *${prefix}wallhacker*
+│❏ヅ *${prefix}wallpubg*
+│❏ヅ *${prefix}wallml*
+└⬣
+
+╭─⬣「 *TEXTPRO* 」⬣
+│❏ヅ *${prefix}halloween* <text>
+│❏ヅ *${prefix}rainbow* <text>
+│❏ヅ *${prefix}scfi* <text>
+│❏ヅ *${prefix}blue* <text>
+│❏ヅ *${prefix}juice* <text>
+│❏ヅ *${prefix}purple* <text>
+│❏ヅ *${prefix}toxic* <text>
+│❏ヅ *${prefix}peridot* <text>
+│❏ヅ *${prefix}metal* <text>
+│❏ヅ *${prefix}realistic* <text>
+│❏ヅ *${prefix}impressive* <text>
+│❏ヅ *${prefix}cracked* <text>
+│❏ヅ *${prefix}magma* <text>
+│❏ヅ *${prefix}thunder* <text>
+│❏ヅ *${prefix}berry* <text>
+│❏ヅ *${prefix}transformer* <text>
+│❏ヅ *${prefix}horror* <text>
+│❏ヅ *${prefix}metallic* <text>
+│❏ヅ *${prefix}circuit* <text>
+│❏ヅ *${prefix}sketch* <text>
+└⬣
+
+╭─⬣「 *RANDOM* 」⬣
+│❏ヅ *${prefix}mygrub* 
+│❏ヅ *${prefix}ceklimit* <text>
+│❏ヅ *${prefix}claim* <text>
+│❏ヅ *${prefix}artinama* <text>
+│❏ヅ *${prefix}artimimpi* <text>
+│❏ヅ *${prefix}resepmasakan* <text>
+│❏ヅ *${prefix}katajago* <text>
+│❏ヅ *${prefix}besarkecil* <text>
+│❏ヅ *${prefix}jumlahhuruf* <text>
+│❏ヅ *${prefix}jumlahangka* <text>
+│❏ヅ *${prefix}infogempa* 
+│❏ヅ *${prefix}balikangka* <text>
+│❏ヅ *${prefix}wikipedia* <text>
+│❏ヅ *${prefix}balikhuruf* <text>
+│❏ヅ *${prefix}bilangangka* <text>
+│❏ヅ *${prefix}holoh* <text>
+│❏ヅ *${prefix}heleh* <text>
+│❏ヅ *${prefix}huluh* <text>
+│❏ヅ *${prefix}hilih* <text>
+│❏ヅ *${prefix}halah* <text>
+│❏ヅ *${prefix}kapital* <text>
+└⬣
+
+╭─⬣「 *ANIME* 」⬣
+│❏ヅ *${prefix}saitama*
+│❏ヅ *${prefix}gon*
+│❏ヅ *${prefix}killua*
+│❏ヅ *${prefix}kakashi*
+│❏ヅ *${prefix}tsunade*
+│❏ヅ *${prefix}orochimaru*
+│❏ヅ *${prefix}mitsuki*
+│❏ヅ *${prefix}sarada*
+│❏ヅ *${prefix}boruto*
+│❏ヅ *${prefix}sakura*
+│❏ヅ *${prefix}sasuke*
+│❏ヅ *${prefix}minato*
+│❏ヅ *${prefix}naruto*
+│❏ヅ *${prefix}copper*
+│❏ヅ *${prefix}nami*
+│❏ヅ *${prefix}ussop*
+│❏ヅ *${prefix}sanji*
+│❏ヅ *${prefix}luffy*
+│❏ヅ *${prefix}zoro*
+│❏ヅ *${prefix}senku*
+│❏ヅ *${prefix}nezuko*
+│❏ヅ *${prefix}tanjirou*
+│❏ヅ *${prefix}natsu*
+│❏ヅ *${prefix}sagiri*
+│❏ヅ *${prefix}rimuru*
+└⬣
+
+╭─⬣「 *WALLPAPER* 」⬣
+│❏ヅ *${prefix}ytmp3*
+│❏ヅ *${prefix}ytmp4*
+│❏ヅ *${prefix}playmp3* [ *ERR* ]
+│❏ヅ *${prefix}playmp4* [ *ERR* ]
+│❏ヅ *${prefix}tiktokmp4*
+│❏ヅ *${prefix}tiktokmp3*
+└⬣
+
+╭─⬣「 *VRANDOM* 」⬣
+│❏ヅ *${prefix}vsadboy*
+│❏ヅ *${prefix}vpakboy*
+│❏ヅ *${prefix}vbaik*
+│❏ヅ *${prefix}vjago*
+│❏ヅ *${prefix}vjelek*
+│❏ヅ *${prefix}vcantik*
+│❏ヅ *${prefix}vpinter*
+│❏ヅ *${prefix}vbeban*
+│❏ヅ *${prefix}vkontol*
+│❏ヅ *${prefix}vhebat*
+│❏ヅ *${prefix}vwibu*
+│❏ヅ *${prefix}vharam*
+│❏ヅ *${prefix}vbabi*
+│❏ヅ *${prefix}vbego*
+│❏ヅ *${prefix}vganteng*
+│❏ヅ *${prefix}vanjing*
+│❏ヅ *${prefix}vmonyet*
+│❏ヅ *${prefix}vsadgirl*
+│❏ヅ *${prefix}vpakgirl*
+│❏ヅ *${prefix}vjahat*
+│❏ヅ *${prefix}vnolep*
+│❏ヅ *${prefix}vgoblok*
+└⬣
+
+╭─⬣「 *ZRANDOM* 」⬣
+│❏ヅ *${prefix}zsadboy*
+│❏ヅ *${prefix}zpakboy*
+│❏ヅ *${prefix}zbaik*
+│❏ヅ *${prefix}zjago*
+│❏ヅ *${prefix}zjelek*
+│❏ヅ *${prefix}zcantik*
+│❏ヅ *${prefix}zpinter*
+│❏ヅ *${prefix}zbeban*
+│❏ヅ *${prefix}zkontol*
+│❏ヅ *${prefix}zhebat*
+│❏ヅ *${prefix}zwibu*
+│❏ヅ *${prefix}zharam*
+│❏ヅ *${prefix}zbabi*
+│❏ヅ *${prefix}zbego*
+│❏ヅ *${prefix}zganteng*
+│❏ヅ *${prefix}zanjing*
+│❏ヅ *${prefix}zmonyet*
+│❏ヅ *${prefix}zsadgirl*
+│❏ヅ *${prefix}zpakgirl*
+│❏ヅ *${prefix}zjahat*
+│❏ヅ *${prefix}znolep*
+│❏ヅ *${prefix}zgoblok*
+└⬣
+
+╭─⬣「 *TAGRANDOM* 」⬣
+│❏ヅ *${prefix}sadboy*
+│❏ヅ *${prefix}pakboy*
+│❏ヅ *${prefix}baik*
+│❏ヅ *${prefix}jago*
+│❏ヅ *${prefix}jelek*
+│❏ヅ *${prefix}cantik*
+│❏ヅ *${prefix}pinter*
+│❏ヅ *${prefix}beban*
+│❏ヅ *${prefix}kontol*
+│❏ヅ *${prefix}hebat*
+│❏ヅ *${prefix}wibu*
+│❏ヅ *${prefix}haram*
+│❏ヅ *${prefix}babi*
+│❏ヅ *${prefix}bego*
+│❏ヅ *${prefix}ganteng*
+│❏ヅ *${prefix}anjing*
+│❏ヅ *${prefix}monyet*
+│❏ヅ *${prefix}sadgirl*
+│❏ヅ *${prefix}pakgirl*
+│❏ヅ *${prefix}jahat*
+│❏ヅ *${prefix}nolep*
+│❏ヅ *${prefix}goblok*
+└⬣
+
+╭─⬣「 *RATERANDOM* 」⬣
+│❏ヅ *${prefix}bapercek*
+│❏ヅ *${prefix}sangecek*
+│❏ヅ *${prefix}pakgirlcek*
+│❏ヅ *${prefix}pakboycek*
+│❏ヅ *${prefix}kontolcek*
+│❏ヅ *${prefix}haramcek*
+│❏ヅ *${prefix}anjingcek*
+│❏ヅ *${prefix}jahatcek*
+│❏ヅ *${prefix}baikcek*
+│❏ヅ *${prefix}bebancek*
+│❏ヅ *${prefix}babicek*
+│❏ヅ *${prefix}nolepcek*
+│❏ヅ *${prefix}jagocek*
+│❏ヅ *${prefix}pintarcek*
+│❏ヅ *${prefix}begocek*
+│❏ヅ *${prefix}goblokcek*
+│❏ヅ *${prefix}jelekcek*
+│❏ヅ *${prefix}cantikcek*
+│❏ヅ *${prefix}gantengcek*
+└⬣
+
+╭─⬣「 *XRANDOM* 」⬣
+│❏ヅ *${prefix}xsadboy* <tag>
+│❏ヅ *${prefix}xpakboy* <tag>
+│❏ヅ *${prefix}xbaik* <tag>
+│❏ヅ *${prefix}xjago* <tag>
+│❏ヅ *${prefix}xjelek* <tag>
+│❏ヅ *${prefix}xcantik* <tag>
+│❏ヅ *${prefix}xpinter* <tag>
+│❏ヅ *${prefix}xbeban* <tag>
+│❏ヅ *${prefix}xkontol* <tag>
+│❏ヅ *${prefix}xhebat* <tag>
+│❏ヅ *${prefix}xwibu* <tag>
+│❏ヅ *${prefix}xharam* <tag>
+│❏ヅ *${prefix}xbabi* <tag>
+│❏ヅ *${prefix}xbego* <tag>
+│❏ヅ *${prefix}xganteng* <tag>
+│❏ヅ *${prefix}xanjing* <tag>
+│❏ヅ *${prefix}xmonyet* <tag>
+│❏ヅ *${prefix}xsadgirl* <tag>
+│❏ヅ *${prefix}xpakgirl* <tag>
+│❏ヅ *${prefix}xjahat* <tag>
+│❏ヅ *${prefix}xnolep* <tag>
+│❏ヅ *${prefix}xgoblok* <tag>
+└⬣
+
+╭─⬣「 *OWNER* 」⬣
+│❏ヅ *${prefix}bc*
+│❏ヅ *${prefix}ban*
+│❏ヅ *${prefix}unban*
+│❏ヅ *${prefix}bcgc*
+│❏ヅ *${prefix}setprefix*
+│❏ヅ *${prefix}settz*
+│❏ヅ *${prefix}join*
+│❏ヅ *${prefix}addprem*
+│❏ヅ *${prefix}dellprem*
+│❏ヅ *${prefix}setppbot*
+│❏ヅ *${prefix}return*
+│❏ヅ *${prefix}setart*
+└⬣
+
+╭─⬣「 *AUDIO* 」⬣
+│❏ヅ *${prefix}audio1*
+│❏ヅ *${prefix}audio2*
+│❏ヅ *${prefix}audio3*
+│❏ヅ *${prefix}audio4*
+│❏ヅ *${prefix}audio5*
+│❏ヅ *${prefix}audio6*
+│❏ヅ *${prefix}audio7*
+│❏ヅ *${prefix}audio8*
+│❏ヅ *${prefix}audio9*
+│❏ヅ *${prefix}audio10*
+│❏ヅ *${prefix}audio11*
+│❏ヅ *${prefix}audio12*
+│❏ヅ *${prefix}audio13*
+│❏ヅ *${prefix}audio14*
+│❏ヅ *${prefix}audio15*
+│❏ヅ *${prefix}audio16*
+│❏ヅ *${prefix}audio17*
+│❏ヅ *${prefix}audio18*
+│❏ヅ *${prefix}audio19*
+│❏ヅ *${prefix}audio20* 
+└⬣
+
+╭─⬣「 *RATETAG* 」⬣
+│❏ヅ *${prefix}bapertag* <tag>
+│❏ヅ *${prefix}sangetag* <tag>
+│❏ヅ *${prefix}pakgirltag* <tag>
+│❏ヅ *${prefix}pakboytag* <tag>
+│❏ヅ *${prefix}kontoltag* <tag>
+│❏ヅ *${prefix}haramtag* <tag>
+│❏ヅ *${prefix}anjingtag* <tag>
+│❏ヅ *${prefix}jahattag* <tag>
+│❏ヅ *${prefix}baiktag* <tag>
+│❏ヅ *${prefix}bebantag* <tag>
+│❏ヅ *${prefix}babitag* <tag>
+│❏ヅ *${prefix}noleptag* <tag>
+│❏ヅ *${prefix}jagotag* <tag>
+│❏ヅ *${prefix}pintartag* <tag>
+│❏ヅ *${prefix}begotag* <tag>
+│❏ヅ *${prefix}gobloktag* <tag>
+│❏ヅ *${prefix}jelektag* <tag>
+│❏ヅ *${prefix}cantiktag* <tag>
+│❏ヅ *${prefix}gantengtag* <tag>
+└⬣
+
+╭─⬣「 *SRANDOM* 」⬣
+│❏ヅ *${prefix}ssadboy*
+│❏ヅ *${prefix}spakboy*
+│❏ヅ *${prefix}sbaik*
+│❏ヅ *${prefix}sjago*
+│❏ヅ *${prefix}sjelek*
+│❏ヅ *${prefix}scantik*
+│❏ヅ *${prefix}spinter*
+│❏ヅ *${prefix}sbeban*
+│❏ヅ *${prefix}skontol*
+│❏ヅ *${prefix}shebat*
+│❏ヅ *${prefix}swibu*
+│❏ヅ *${prefix}sharam*
+│❏ヅ *${prefix}sbabi*
+│❏ヅ *${prefix}sbego*
+│❏ヅ *${prefix}sganteng*
+│❏ヅ *${prefix}sanjing*
+│❏ヅ *${prefix}smonyet*
+│❏ヅ *${prefix}ssadgirl*
+│❏ヅ *${prefix}spakgirl*
+│❏ヅ *${prefix}sjahat*
+│❏ヅ *${prefix}snolep*
+│❏ヅ *${prefix}sgoblok*
+└⬣
+
+╭─⬣「 *CRANDOM* 」⬣
+│❏ヅ *${prefix}csadboy*
+│❏ヅ *${prefix}cpakboy*
+│❏ヅ *${prefix}cbaik*
+│❏ヅ *${prefix}cjago*
+│❏ヅ *${prefix}cjelek*
+│❏ヅ *${prefix}ccantik*
+│❏ヅ *${prefix}cpinter*
+│❏ヅ *${prefix}cbeban*
+│❏ヅ *${prefix}ckontol*
+│❏ヅ *${prefix}chebat*
+│❏ヅ *${prefix}cwibu*
+│❏ヅ *${prefix}charam*
+│❏ヅ *${prefix}cbabi*
+│❏ヅ *${prefix}cbego*
+│❏ヅ *${prefix}cganteng*
+│❏ヅ *${prefix}canjing*
+│❏ヅ *${prefix}cmonyet*
+│❏ヅ *${prefix}csadgirl*
+│❏ヅ *${prefix}cpakgirl*
+│❏ヅ *${prefix}cjahat*
+│❏ヅ *${prefix}cnolep*
+│❏ヅ *${prefix}cgoblok*
+└⬣
+
+╭─⬣「 *SEARCH* 」⬣
+│❏ヅ *${prefix}grubwa* <text>
+│❏ヅ *${prefix}arena* <text>
+│❏ヅ *${prefix}amazon* <text>
+│❏ヅ *${prefix}shopee* <text>
+│❏ヅ *${prefix}thelazy* <text>
+│❏ヅ *${prefix}cersex* <text>
+│❏ヅ *${prefix}ytsearch* <text>
+│❏ヅ *${prefix}rexdl* <text>
+│❏ヅ *${prefix}mod* <text>
+│❏ヅ *${prefix}sfile* <text>
+└⬣
+
+╭─⬣「 *NEWSS* 」⬣
+│❏ヅ *${prefix}antara*
+│❏ヅ *${prefix}okezone*
+│❏ヅ *${prefix}kompas*
+└⬣
+
+╭─⬣「 *INFORMATION* 」⬣
+│❏ヅ *${prefix}jam*
+│❏ヅ *${prefix}jamdunia*
+│❏ヅ *${prefix}jadwalbola*
+│❏ヅ *${prefix}infohoax*
+│❏ヅ *${prefix}coronameninggal*
+└⬣
